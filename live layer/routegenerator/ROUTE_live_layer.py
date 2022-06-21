5932d767632927fecfca02e88f859431a35190fb
DamiAkinniyi <oluwadamilola.akinniyi@students.iaac.net>
2022-06-21 09:14:35 -0500

    Updated to final Route Generation

--- live layer/routegenerator/ROUTE_live_layer.py
+++ live layer/routegenerator/ROUTE_live_layer.py
@@ -3,6 +3,8 @@ import networkx as nx
 import osmnx as ox
 import geopandas as gpd
 import pandas as pd
+import fiona
+fiona.drvsupport.supported_drivers['WFS'] = 'r'
 import momepy
 import shapely
 
@@ -10,26 +12,22 @@ from random import random
 
 
 
-def call(age:str, gender:str, ethnicity:str, religion:str, special_needs_visual:bool,special_needs_physical:bool, with_child:bool, formality:float, public_image:float, origin = (48.19, 16.362), destination = (48.20, 16.364), call_type='poi' ):
+def call(special_needs_visual:bool,special_needs_physical:bool,  public_image:float, origin = (48.19, 16.362), destination = (48.20, 16.364), ):
     '''takes in user profile and preferences and returns either preferred poi or generated route
     
     args:
-    age         : Select from "young", "adult", "old"
-    gender      : drop_down_options = ('male', 'female', 'other')
-    ethnicity   : checkbox_options = ('afro_carribean', 'asian', middle_eastern', 'south_asian', 'latin_american', 'european_origin')
-    religion    : drop_down options = ('christian', 'muslim', 'jewish', 'buddhist', 'hinduism', 'others', 'none' )#Multiple options allowed for ethnicity
     special_needs_visual    : Get boolean from a check box
     special_needs_physical  : Get boolean from a check box
-    with_child  : Get boolean from a check box
-    formality   : Slider with range of 0 to 1
     public_image    : Slider with range of 0 to 1
+    origin  : Starting location specified as a tuple of (lat, lon)
+    destination  : target location specified as a tuple of (lat, lon)
 
-    returns : poi call returns geojson with Point geometry
+    returns : returns shortest path geojson with Linestring geometry
     '''
     
     #-----------------------------------CITY STREETS DATAFRAME---------------------------------------#
     #GET PUBLIC IMAGE DATA
-    urban_image_url = 'https://drive.google.com/file/d/1-KDBSknNJ04vhp1I1xHILhZ7pJeRQDuP/view?usp=sharing'
+    urban_image_url = 'https://drive.google.com/file/d/1-jV3M-MaPq_EKyezH7-etr4a0y-616BY/view?usp=sharing'
     urban_image_url='https://drive.google.com/uc?id=' + urban_image_url.split('/')[-2]
     urban_image = gpd.read_file(urban_image_url)
 
@@ -44,12 +42,21 @@ def call(age:str, gender:str, ethnicity:str, religion:str, special_needs_visual:
     acc_wheelchair = gpd.read_file(acc_wheelchair_url)
 
     #GET CITY EDGES INFORMATION INTO ONE DATAFRAME
-    city_edges = urban_image.loc[:, ['name', 'weighted_sum']]
+    city_edges = urban_image.loc[:, ['u', 'v', 'key', 'name', 'weighted_sum']]
     city_edges.weighted_sum = 1-city_edges.weighted_sum
     city_edges['blind_friendly'] = 1-acc_visual['visual_friendly_index']
     city_edges['wheelchair_friendly'] = 1-acc_wheelchair['wheelchair_friendly_index']
     city_edges['geometry'] = acc_wheelchair['geometry']
     city_edges = city_edges.rename(columns={'weighted_sum':'urban_image'})
+    city_edges = city_edges.set_index(['u', 'v', 'key'])
+
+    #GET CITY NODES INFORMATION INTO ONE DATAFRAME
+    city_nodes_url = 'https://drive.google.com/file/d/100i_xMPGcs2ouBeT0TlOxzNpywE0wsUU/view?usp=sharing'
+    city_nodes_url='https://drive.google.com/uc?id=' + city_nodes_url.split('/')[-2]
+    city_nodes = gpd.read_file(city_nodes_url)
+    city_nodes = city_nodes.set_index('osmid')
+
+
     #-----------------------------------CITY STREETS DATAFRAME---------------------------------------#
 
     #-----------------------------------AFFECT STREET WEIGHTS WITH USER INPUT---------------------------------------#
@@ -74,26 +81,52 @@ def call(age:str, gender:str, ethnicity:str, religion:str, special_needs_visual:
         routes_edges.wheelchair_friendly = 1.0
 
     routes_edges['edge_weights'] = routes_edges.urban_image + routes_edges.blind_friendly + routes_edges.wheelchair_friendly
+
+    new_cols = [col for col in routes_edges.columns.values if col != 'geometry'] + ['geometry']
+    routes_edges = routes_edges.reindex(new_cols, axis=1)
     #-----------------------------------AFFECT STREET WEIGHTS WITH USER INPUT---------------------------------------#
 
-    
+    #-----------------------------------COMBINE GRAPH AND PLOT ROUTES---------------------------------------#
+    graph = ox.graph_from_gdfs(city_nodes, routes_edges )
 
+    #make a copy of the graph for reference
+    gcopy = graph.copy()
 
-#DEFINE USER INPUTS FOR TESTING
-#PROFILE
-age = 'adult' # Select from "young", "adult", "old"
-gender = 'female' #drop_down_options = ('male', 'female', 'other')
-ethnicity = ['latin_american'] #checkbox_options = ('afro_carribean', 'asian', middle_eastern', 'south_asian', 'latin_american', 'european_origin')
-religion = 'muslim' #drop_down options = ('christian', 'muslim', 'jewish', 'buddhist', 'hinduism', 'others', 'none' )
-#Multiple options allowed for ethnicity
-special_needs_visual = False #Get boolean from a check box
-special_needs_physical = False # Get boolean from a check box
-with_child = True # Get boolean from a check box
+    #Get source and target locations to nearast node
+    source = ox.distance.nearest_nodes(graph, origin[1], origin[0])
+    target = ox.distance.nearest_nodes(graph, destination[1], destination[0])
 
-#PREFERENCES
-formality = 0 #Slider with range of 0 to 1
-public_image = 0.5 #Slider with range of 0 to 1
+    #construct path. returns nodes that make up the path
+    path = nx.dijkstra_path(graph, source, target, weight='edge_weights')
 
-#CALL
-call_type = 'poi'
-call(age, gender, ethnicity, religion, special_needs_visual, special_needs_physical, with_child, formality, public_image)
+    #construct position for nodes
+    pos = {i:[graph.nodes[i]['x'], graph.nodes[i]['y']]  for i in path }
+
+    #construct edge for the path
+    edge_path=[]
+    for i in range(len(path)-1):
+        edge=(path[i],path[i+1])
+        edge_path.append(edge)
+
+    #construct graph for path
+    graph_path=nx.Graph()
+    graph_path.add_nodes_from(path)
+    graph_path.add_edges_from(edge_path)
+
+    #construct geometry for path
+    graph_path.edges(data=True)
+    for u,v,data in graph_path.edges(data=True):
+        data['geometry'] = shapely.geometry.LineString([pos[u], pos[v]])     
+
+    #assign x,y to nodes 
+    for u,data in graph_path.nodes(data=True):
+        data['x'] = pos[u][0]
+        data['y'] = pos[u][1]
+
+    #convert edges into geodataframe
+    route_edges = momepy.nx_to_gdf(graph_path, points=False)
+
+    #convert geodataframe to geojson
+    route_output = route_edges.to_json()
+
+    return route_output
